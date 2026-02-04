9fb782b4219a5e7232d97952b8a83ffd654952ad
vika <kushkinavika3@gmail.com>
2026-02-04 17:51:43 +0300

    -

--- tilesets/пппп.tsx
+++ tilesets/пппп.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="пппп" tilewidth="32" tileheight="32" tilecount="900" columns="30">
- <image source="Downloads/hyptosis_tile-art-batch-4.png" width="960" height="960"/>
+ <image source="tilesets/hyptosis_tile-art-batch-4.png" width="960" height="960"/>
 </tileset>
