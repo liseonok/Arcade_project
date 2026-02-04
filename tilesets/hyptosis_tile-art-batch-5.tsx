9a45ba4b94c1ef80d83d7fa7a6d0e038b2a4ffda
vika <kushkinavika3@gmail.com>
2026-02-04 18:02:36 +0300

    -

--- tilesets/hyptosis_tile-art-batch-5.tsx
+++ tilesets/hyptosis_tile-art-batch-5.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="hyptosis_tile-art-batch-5" tilewidth="32" tileheight="32" tilecount="722" columns="19">
- <image source="tilesets/hyptosis_tile-art-batch-5.png" width="636" height="1219"/>
+ <image source="hyptosis_tile-art-batch-5.png" width="636" height="1219"/>
 </tileset>
